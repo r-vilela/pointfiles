3fed6b60378d353f031a53c3eb0cadb61074c407
TUF_garoa <raulvilela29@gmail.com>
2025-04-20 21:31:33 -0400

    fix: fixing pull on fugitive

--- .config/nvim/lua/garoa/config/fugitive.lua
+++ .config/nvim/lua/garoa/config/fugitive.lua
@@ -22,7 +22,7 @@ return {
                 end,opts)
 
                 vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({'pull', '--rebase'})
+                    vim.cmd("Git pull --rebase")
                 end, opts)
 
                 vim.keymap.set("n", "<leader>t", ":Git push -u origin", opts);
